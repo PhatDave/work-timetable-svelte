22b9a174f5fb772a22ac18bf2860a4ea6fe35b8e
David Majdandžić <david.majdandzic@hotmail.com>
2023-06-10 14:04:14 +0200

    Bump versions

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -3,38 +3,38 @@ const heropatterns = require('tailwindcss-hero-patterns/src/patterns');
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ['src/**/*.{html,js,svelte,ts}'],
-  darkMode: 'class',
+	content: ['src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
 
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['roboto', ...defaultTheme.fontFamily.sans]
-      }
-    },
-    heroPatternsOpacities: ['10'],
-    heroPatterns: {
-      topography: heropatterns.topography
-    }
-  },
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['roboto', ...defaultTheme.fontFamily.sans]
+			}
+		},
+		heroPatternsOpacities: ['10'],
+		heroPatterns: {
+			topography: heropatterns.topography
+		}
+	},
 
-  daisyui: {
-    themes: [
-      {
-        light: {
-          ...require('daisyui/src/colors/themes')['[data-theme=lofi]'],
-          primary: '#45B1E8',
-          secondary: '#fbbf24'
-        },
-        dark: {
-          ...require('daisyui/src/colors/themes')['[data-theme=black]'],
-          primary: '#45B1E8',
-          secondary: '#fbbf24'
-        }
-      }
-    ]
-  },
-  plugins: [require('daisyui'), require('tailwindcss-hero-patterns')]
+	daisyui: {
+		themes: [
+			{
+				light: {
+					...require('daisyui/src/theming/themes')['[data-theme=lofi]'],
+					primary: '#45B1E8',
+					secondary: '#fbbf24'
+				},
+				dark: {
+					...require('daisyui/src/theming/themes')['[data-theme=black]'],
+					primary: '#45B1E8',
+					secondary: '#fbbf24'
+				}
+			}
+		]
+	},
+	plugins: [require('daisyui'), require('tailwindcss-hero-patterns')]
 };
 
 module.exports = config;
